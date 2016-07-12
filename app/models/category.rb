b255878eb9c99001e2d4839e16fe67ec58d32633
Kellye Greene <kellye@gust.com>
2016-07-11 20:02:57 -0400

    displaying associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,20 @@ class Category < ActiveRecord::Base
 
   has_many :posts
 
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+  
+  def create
+    @category.create!(params[:name])
+  end
+
+  def new
+    @category=Category.new
+  end
+
 end
